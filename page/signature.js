0619e13b5e9f4102ec3f2d52a5ab2487e6e5f239
jiwjung <jwjung2004@naver.com>
2024-02-05 20:17:35 +0900

    Update signature.js

--- page/signature.js
+++ page/signature.js
@@ -30,7 +30,6 @@ document.getElementById('submit_button').addEventListener('click', async () => {
         studentPhoneNumber: student_phone_number,
         URL: URL
     }
-    await axios.post('http://jwjung.kro.kr:20000/register', json);
     const response = await axios.post('http://jwjung.kro.kr:20000/register', json);
     if (!response.isIn) {
         alert("졸업자 명단에 없는 학번과 이름입니다.");
