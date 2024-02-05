6eb4c9a9ce7d25b13e97f2592f7db89c3cf64963
KimMinme <minme3859@gmail.com>
2024-02-05 19:09:25 +0900

    Logic to reject unusable data
    data 1: student_id
    data 2: student_phone_number
    data 3: signature

--- page/signature.js
+++ page/signature.js
@@ -5,12 +5,24 @@ document.getElementById('clear').addEventListener('click', () => {
 });
 
 document.getElementById('submit_button').addEventListener('click', async () => {
+    const student_id = document.getElementById('student_id').value;
+    if (student_id.length != 9) {
+        alert("학번이 유효하지 않습니다.");
+        return;
+    }
+    const student_phone_number = document.getElementById('student_phone_number').value;
+    if (student_phone_number.length != 11) {
+        alert("전화번호가 유효하지 않습니다.");
+        return;
+    }
+    if (signaturePad.isEmpty()) {
+        alert("서명해 주십시오.");
+        return;
+    }
     const willSubmit = confirm("제출하시겠습니까?");
     if (willSubmit == false) return;
 
-    const student_id = document.getElementById('student_id').value;
     const student_name = document.getElementById('student_name').value;
-    const student_phone_number = document.getElementById('student_phone_number').value;
     const URL = signaturePad.toDataURL();
     const json = {
         studentId: student_id,
@@ -19,6 +31,11 @@ document.getElementById('submit_button').addEventListener('click', async () => {
         URL: URL
     }
     await axios.post('http://jwjung.kro.kr:8080/register', json);
+    const response = await axios.post('http://jwjung.kro.kr:80/register', json);
+    if (!response.isIn) {
+        alert("졸업자 명단에 없는 학번과 이름입니다.");
+        return;
+    }
 
     alert("제출되었습니다.");
     document.querySelector('form').reset();
